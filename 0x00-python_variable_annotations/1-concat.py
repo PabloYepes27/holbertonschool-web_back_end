a30ae4df6f3a12c939ccc4bfcd261532c821d7c8
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-06 09:18:49 -0500

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" File that contains a concatenation string function """
+
+def concat(str1:str, str2:str) -> str:
+    """
+    Description:
+        Function that returns a concatenated string
+
+    Args:
+        str1 (str): first number
+        str2 (str): second number
+
+    Returns:
+        a concatenated string
+    """
+    return str1 + str2

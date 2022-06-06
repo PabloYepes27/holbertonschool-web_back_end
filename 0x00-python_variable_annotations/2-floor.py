620e33dbea8efe32b91135e4f283c488f90aa71f
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-06 09:25:37 -0500

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" File that contains a floor function """
+
+def floor(n:float) -> int:
+    """
+    Description:
+        Function that returns the floor of a float number
+
+    Args:
+        n (float): float number
+
+    Returns:
+        floor of a number
+    """
+    return int(n)

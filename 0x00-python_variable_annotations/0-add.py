4860c2ad10d7745936ffcce7609ffd1584e6f293
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-06 09:11:53 -0500

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# File that contains an add function
+
+def add(a:float, b:float) -> float:
+    """
+    Function that returns the sum of two float numbers
+
+    Args:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        float sum of two numbers
+    """
+    return a + b

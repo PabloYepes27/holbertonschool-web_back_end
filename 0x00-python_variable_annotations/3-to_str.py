aaa3d77dbce5ae69e51fe2003ab8ef1ccca62b83
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-06 09:28:25 -0500

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" File that contains a to string function """
+
+def to_str(n:float) -> str:
+    """
+    Description:
+        Function that returns the string representation of a float number
+
+    Args:
+        n (float): float number
+
+    Returns:
+        string representation of a float number
+    """
+    return str(n)

4ef63734b1d40f8639b4d3a9015e82b6b79454b8
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-06 18:11:26 -0500

    mamahuevo

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
-""" File that contains a make_multiplier function """
+"""File that contains make_multiplier function"""
+
 
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """
-        Function that returns a function that multiplies two floats
-
-        Args:
-            multiplier (float): a float number
-
-        Return:
-            Callable[[float], float]: function that multiplies a float by multiplier.
-    """
+    """Return multiplier by other float number"""
     return lambda n: n * multiplier

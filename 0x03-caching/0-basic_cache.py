bcf7cc4adb7750f6249ed89a6d92897728ebd75f
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-10 10:36:16 -0500

    fix

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,14 +1,22 @@
-#!/usr/bin/python3
-""" BaseCache module"""
+#!/usr/bin/env python3
+"""
+Base Caching System
+"""
+
+
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """class BasicCache that inherits from BaseCaching and is a caching system"""
+    """Base Cache system
+    """
     def put(self, key, item):
-        """ Add an item in the cache"""
+        """ Add an item in the cache
+        """
         if key and item:
             self.cache_data[key] = item
-    
+
     def get(self, key):
-        """Get an item by key"""
+        """ Get an item by key
+        """
         return self.cache_data.get(key)

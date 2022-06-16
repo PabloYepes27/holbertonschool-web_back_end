e9d685d8e0148c5c8fa594e7d13087b3b9a8114b
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-16 08:46:28 -0500

    2. Create logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
 """" Module named filtered_logger """
-
 import re
 from typing import List
 import logging
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """ Create logger
+    Return: a logging.Logger object
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
-
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-
     def __init__(self, fields: List[str]):
         """ Init constructor """
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
-
     def format(self, record: logging.LogRecord) -> str:
         """ Implement the format method to filter values in incoming log
         records using filter_datum()
         """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
-
-
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Function that returns the log message obfuscated

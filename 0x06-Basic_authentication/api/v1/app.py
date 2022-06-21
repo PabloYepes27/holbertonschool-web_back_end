a67ea9ff2830e5d64cc4ff79d13795739ba21387
“PabloYepes27” <juanpabloyepest@gmail.com>
2022-06-21 06:25:26 -0500

    7. Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -19,6 +19,7 @@ elif (os.getenv("AUTH_TYPE") == "basic_auth"):
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
